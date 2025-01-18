d2de307a75433b9afa6cf35f83e0d72facc21051
swapniltw-tech <swapnil.marathe@thoughtworks.com>
2025-01-18 19:18:12 +0530

    Sample test for aerospike data settings and connection settings

--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/SimpleSpringbootAerospikeDemoApplication.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/SimpleSpringbootAerospikeDemoApplication.java
@@ -1,13 +1,34 @@
 package com.aerospike.demo.simplespringbootaerospikedemo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.aerospike.config.AerospikeConnectionSettings;
+import org.springframework.data.aerospike.config.AerospikeDataSettings;
 
 @SpringBootApplication
-public class SimpleSpringbootAerospikeDemoApplication {
+@Slf4j
+public class SimpleSpringbootAerospikeDemoApplication implements ApplicationRunner {
+
+	@Autowired
+	AerospikeDataSettings aerospikeDataSettings;
+
+	@Autowired
+	AerospikeConnectionSettings aerospikeConnectionSettings;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSpringbootAerospikeDemoApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.info("AerospikeDataSettings scan enabled {}", aerospikeDataSettings.isScansEnabled());
+		log.info("AerospikeDataSettings create indexes enabled {}", aerospikeDataSettings.isCreateIndexesOnStartup());
+		log.info("AerospikeDataSettings create indexes enabled {}", aerospikeDataSettings.isCreateIndexesOnStartup());
+		log.info("AerospikeDataSettings  index cache refresh seconds {}", aerospikeDataSettings.getIndexCacheRefreshSeconds());
+		log.info("AerospikeConnectionSettings  namespace {}", aerospikeConnectionSettings.getNamespace());
+	}
 }
