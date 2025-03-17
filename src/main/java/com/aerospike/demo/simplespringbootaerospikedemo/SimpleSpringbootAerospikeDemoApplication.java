56707992de51fcb5d50ec905ebd0254a530e85eb
agrgr <agrgr@users.noreply.github.com>
2025-03-17 16:55:16 +0200

    update Spring Data Aerospike to 5.0.1

--- src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/SimpleSpringbootAerospikeDemoApplication.java
+++ src/main/java/com/aerospike/demo/simplespringbootaerospikedemo/SimpleSpringbootAerospikeDemoApplication.java
@@ -30,6 +30,7 @@ public class SimpleSpringbootAerospikeDemoApplication implements ApplicationRunn
 		log.info("AerospikeDataSettings create indexes enabled {}", aerospikeDataSettings.isCreateIndexesOnStartup());
 		log.info("AerospikeDataSettings  index cache refresh seconds {}", aerospikeDataSettings.getIndexCacheRefreshSeconds());
 		log.info("AerospikeDataSettings  queryMaxRecords {}", aerospikeDataSettings.getQueryMaxRecords());
-		log.info("AerospikeConnectionSettings  namespace {}", aerospikeConnectionSettings.getNamespace());
+		log.info("AerospikeDataSettings  queryMaxRecords {}", aerospikeDataSettings.getNamespace());
+		log.info("AerospikeConnectionSettings  namespace {}", aerospikeConnectionSettings.getHosts());
 	}
 }
